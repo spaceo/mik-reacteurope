9d1cceff10ef451308ed89e8a972dc6c52c6dd6a
Mikkel Jakobsen <mikkel@adapt.dk>
2017-05-16 13:47:37 +0200

    My changes

--- src/withDefaultCountProp.js
+++ src/withDefaultCountProp.js
@@ -1,1 +1,9 @@
 import React from 'react';
+
+const withDefaultCountProp = (Component, count) => {
+  return (props) => {
+    return <Component count={count} {...props} />
+  }
+}
+
+export default withDefaultCountProp
