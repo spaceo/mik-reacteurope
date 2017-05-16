c731d2dca8ca2012c1d47dade3ddd6893a161e8e
Mikkel Jakobsen <mikkel@adapt.dk>
2017-05-16 13:42:50 +0200

    My changes

--- src/withDefaultCountPropNine.js
+++ src/withDefaultCountPropNine.js
@@ -1,1 +1,9 @@
 import React from 'react';
+
+const withDefaultCountPropNine = (Component) => {
+  return (props) => {
+    return <Component count={9} {...props} />
+  }
+}
+
+export default withDefaultCountPropNine
