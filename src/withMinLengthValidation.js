1e171f22143c440878898608dedd2823c0c3d1e8
Mikkel Jakobsen <mikkel@adapt.dk>
2017-05-16 15:21:45 +0200

    My changes

--- src/withMinLengthValidation.js
+++ src/withMinLengthValidation.js
@@ -1,1 +1,14 @@
 import React from 'react';
+
+const withMinLengthValidation = (Component, min) => {
+  return (props) => {
+    return (
+      <div>
+        <Component {...props} />
+        {props.value.toString().length < min && <span>Minimum {min} required.</span>}
+      </div>
+    )
+  }
+}
+
+export default withMinLengthValidation
